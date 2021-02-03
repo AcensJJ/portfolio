afd77f032ed4e143a118797c58c5c4c61c35be5a
AcensJJ <uentosacens@gmail.com>
2021-02-03 20:24:02 +0100

    loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$(window).on('load', function() { // makes sure the whole site is loaded 
+	$('#status').fadeOut(); // will first fade out the loading animation 
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	$('body').delay(350).css({'overflow':'visible'});
+})
+
 //capture scroll any percentage
 $(window).scroll(function(){
 	var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
