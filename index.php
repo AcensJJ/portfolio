d6d42c867e07adeb1fb1cde37eb5d6fab261b33a
AcensJJ <uentosacens@gmail.com>
2021-02-04 17:33:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -189,7 +189,7 @@
 					</div>
 				</div>
 				<div class="column right">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2931.976074031167!2d2.8872093153338927!3d42.704227379165175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b06e50e83c46ef%3A0x979ac573b8915819!2s34%20Rue%20Henri%20Bataille%2C%2066000%20Perpignan!5e0!3m2!1sfr!2sfr!4v1612367492828!5m2!1sfr!2sfr" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+					<!-- IMG -->
 				</div>
 			</div>
 		</div>
