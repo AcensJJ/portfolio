13ac1d92d097902e431d6452aba801e3ee0b7407
AcensJJ <uentosacens@gmail.com>
2021-06-08 16:18:26 +0200

    carousel swap

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,6 +1,5 @@
-
 <!-- Flickity HTML init -->
-<div class="carousel" data-flickity>
+<div class="carousel" data-flickity='{ "wrapAround": true, "autoPlay": 3000 }'>
   <div class="carousel-cell"></div>
   <div class="carousel-cell"></div>
   <div class="carousel-cell"></div>
