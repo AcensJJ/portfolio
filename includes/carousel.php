e2ac93a2000b8370751f1fe6b2e08be6e8696f0f
AcensJJ <uentosacens@gmail.com>
2021-06-09 03:16:05 +0200

    carousel projet

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,11 +1,9 @@
 <!-- Flickity HTML init -->
 <section class="carousel-sec">
 	<div class="carousel fade-in" data-flickity='{ "imagesLoaded": true, "percentPosition": false, "wrapAround": true, "autoPlay": 3000 }'>
-		<a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" alt="orange tree" /></a>
-		<a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" alt="submerged" /></a>
-		<a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" alt="look-out" /></a>
-		<a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" alt="One World Trade" /></a>
-		<a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" alt="drizzle" /></a>
-		<a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" alt="cat nose" /></a>
+		<a href="https://github.com/AcensJJ/friends"><img src="https://user-images.githubusercontent.com/45235527/96745102-0ef01700-13c6-11eb-87ef-cdc793c7277e.PNG" alt="Sosy" /></a>
+		<a href="https://github.com/AcensJJ/ppe2"><img src="https://user-images.githubusercontent.com/45235527/96803820-9dd45200-140d-11eb-89c6-a159b507ee0a.PNG" alt="PPE2" /></a>
+		<a href="https://github.com/AcensJJ/cub3d"><img src="https://user-images.githubusercontent.com/45235527/104591011-67088380-566c-11eb-8374-ba15225da9f5.gif" alt="Cub3D" /></a>
+		<a href="https://github.com/AcensJJ/PPE3"><img src="https://user-images.githubusercontent.com/45235527/96742800-938d6600-13c3-11eb-998a-874680b5f667.PNG" alt="PPE3" /></a>
 	</div>
 </section>
