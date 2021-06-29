5f2624dd996ec3dd0ea6a718a405c8e89d4f0f31
AcensJJ <uentosacens@gmail.com>
2021-06-29 18:08:35 +0200

    Update carousel.php

--- includes/carousel.php
+++ includes/carousel.php
@@ -3,7 +3,7 @@
 	<div class="carousel fade-in" data-flickity='{ "imagesLoaded": true, "percentPosition": false, "wrapAround": true, "autoPlay": 3000 }'>
 		<a href="https://github.com/AcensJJ/friends"><img src="https://user-images.githubusercontent.com/45235527/96745102-0ef01700-13c6-11eb-87ef-cdc793c7277e.PNG" alt="Sosy" /></a>
 		<a href="https://github.com/AcensJJ/ppe2"><img src="https://user-images.githubusercontent.com/45235527/96803820-9dd45200-140d-11eb-89c6-a159b507ee0a.PNG" alt="PPE2" /></a>
-		<a href="https://github.com/AcensJJ/cub3d"><img src="https://user-images.githubusercontent.com/45235527/104591011-67088380-566c-11eb-8374-ba15225da9f5.gif" alt="Cub3D" /></a>
+		<a href="https://github.com/AcensJJ/cub3d"><img src="https://user-images.githubusercontent.com/45235527/104811336-f2b01a80-57fa-11eb-9866-985ac5ca8f3a.png" alt="Cub3D" /></a>
 		<a href="https://github.com/AcensJJ/PPE3"><img src="https://user-images.githubusercontent.com/45235527/96742800-938d6600-13c3-11eb-998a-874680b5f667.PNG" alt="PPE3" /></a>
 	</div>
 </section>
