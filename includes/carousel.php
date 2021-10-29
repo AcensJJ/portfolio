108b63096df646cf15b79e5872d54b2dc6793e87
AcensJJ <uentosacens@gmail.com>
2021-10-29 19:14:40 +0200

    pong

--- includes/carousel.php
+++ includes/carousel.php
@@ -5,5 +5,6 @@
 		<a href="https://github.com/AcensJJ/ppe2"><img src="https://user-images.githubusercontent.com/45235527/96803820-9dd45200-140d-11eb-89c6-a159b507ee0a.PNG" alt="PPE2" /></a>
 		<a href="https://github.com/AcensJJ/cub3d"><img src="https://user-images.githubusercontent.com/45235527/104811336-f2b01a80-57fa-11eb-9866-985ac5ca8f3a.png" alt="Cub3D" /></a>
 		<a href="https://github.com/AcensJJ/PPE3"><img src="https://user-images.githubusercontent.com/45235527/96742800-938d6600-13c3-11eb-998a-874680b5f667.PNG" alt="PPE3" /></a>
+		<a href="https://github.com/AcensJJ/ft_transcendence"><img src="https://user-images.githubusercontent.com/45235527/139443999-fda6d2cf-2a95-4b9e-9d57-d77d25e604da.png" alt="Pong War" /></a>
 	</div>
 </section>
