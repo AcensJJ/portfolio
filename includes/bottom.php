b00490c10ead2b7380b317eb94cb79a22294eac2
AcensJJ <uentosacens@gmail.com>
2022-01-05 18:18:16 +0100

    remove bs script

--- includes/bottom.php
+++ includes/bottom.php
@@ -4,7 +4,6 @@
 		<script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/gsap-latest-beta.min.js'></script>
 		<!-- cv js -->
 		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
-		<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
 		<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.js'></script>
 		<!-- --- -->
 
